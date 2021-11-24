3578c08b3616e04442b09a07a523cb3a9b552a83
lliurex <juanma1980@gmail.com>
2021-11-24 11:44:03 +0100

    Disable sanitize string as it must be sanitized in previous steps

--- rebost/plugins/sqlHelper.py
+++ rebost/plugins/sqlHelper.py
@@ -183,9 +183,9 @@ class sqlHelper():
 				for data in cursor.fetchall():
 					(pkgname,value)=data
 					json_value=json.loads(value)
-					json_value['description']=rebostHelper._sanitizeString(json_value['description'])
-					json_value['summary']=rebostHelper._sanitizeString(json_value['summary'])
-					json_value['name']=rebostHelper._sanitizeString(json_value['name'])
+					#json_value['description']=rebostHelper._sanitizeString(json_value['description'])
+					#json_value['summary']=rebostHelper._sanitizeString(json_value['summary'])
+					#json_value['name']=rebostHelper._sanitizeString(json_value['name'])
 					value=str(json.dumps(json_value))
 					query="SELECT * FROM {} WHERE pkg LIKE '{}'".format(main_tmp_table,pkgname)
 					row=main_cursor.execute(query).fetchone()
@@ -203,9 +203,9 @@ class sqlHelper():
 							elif isinstance(item,str) and isinstance(json_main_value.get(key,None),str):
 								if len(item)>len(json_main_value.get(key,'')):
 									json_main_value[key]=item
-						json_main_value['description']=rebostHelper._sanitizeString(json_main_value['description'])
-						json_main_value['summary']=rebostHelper._sanitizeString(json_main_value['summary'])
-						json_main_value['name']=rebostHelper._sanitizeString(json_main_value['name'])
+						#json_main_value['description']=rebostHelper._sanitizeString(json_main_value['description'])
+						#json_main_value['summary']=rebostHelper._sanitizeString(json_main_value['summary'])
+						#json_main_value['name']=rebostHelper._sanitizeString(json_main_value['name'])
 
 						value=str(json.dumps(json_main_value))
 						query="UPDATE {} SET data='{}' WHERE pkg='{}';".format(main_tmp_table,value,pkgname)

a669450757c3ffc9ea4141ac24698d316b19139e
lliurex <juanma1980@gmail.com>
2021-12-09 12:02:45 +0100

    check tmp

--- rebost/helper/rebost-software-manager.sh
+++ rebost/helper/rebost-software-manager.sh
@@ -2,8 +2,15 @@
 #License GPL-3
 #Copyright 2021 LliureX Team
 /usr/sbin/epi-gtk -nc $1
-tmpDir=$(dirname $1)
-if [[ tmpDir":x" != ":x" ]]
+TMPDIR=$(dirname $1)
+#Check if what is told to remove is also what we want to remove
+if [ -e $1 ]
 then
-	rm -r $tmpDir
+	if [[ ${TMPDIR:0:8} == "/tmp/tmp" ]]
+	then
+		if [[ ${1/*./.} == ".epi" ]]
+		then 
+			rm -r $TMPDIR
+		fi
+	fi
 fi

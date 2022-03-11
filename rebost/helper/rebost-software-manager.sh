f9f1f30cd210de8fc1285188614d7103ce40f8ca
lliurex <juanma1980@gmail.com>
2022-03-11 12:16:34 +0100

    don't remove tmpfiles

--- rebost/helper/rebost-software-manager.sh
+++ rebost/helper/rebost-software-manager.sh
@@ -4,13 +4,13 @@
 /usr/sbin/epi-gtk -nc $1
 TMPDIR=$(dirname $1)
 #Check if what is told to remove is also what we want to remove
-if [ -e $1 ]
-then
-	if [[ ${TMPDIR:0:8} == "/tmp/tmp" ]]
-	then
-		if [[ ${1/*./.} == ".epi" ]]
-		then 
-			rm -r $TMPDIR
-		fi
-	fi
-fi
+####if [ -e $1 ]
+####then
+####	if [[ ${TMPDIR:0:8} == "/tmp/tmp" ]]
+####	then
+####		if [[ ${1/*./.} == ".epi" ]]
+####		then 
+####			rm -r $TMPDIR
+####		fi
+####	fi
+####fi

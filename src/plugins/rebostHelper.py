490eddc37f65b96ce75b4ac199c54143d7f7fc81
lliurex <juanma1980@gmail.com>
2025-06-18 12:34:29 +0200

    discard common words as candidate names

--- src/plugins/rebostHelper.py
+++ src/plugins/rebostHelper.py
@@ -381,15 +381,15 @@ def _appstreamToRebost(appstreamCatalogue):
 def _componentGetName(component):
 	name=component.get_id()
 	nameComponents=name.lower().split(".")
-	cont=len(nameComponents)-1
-	banlist=["desktop","org","net","com","app"]
-	name=nameComponents[-1].lower()
-	while cont>=0:
-		if nameComponents[cont].lower() not in banlist and len(nameComponents[cont])>1:
-			name=nameComponents[cont].lower()
-			name=name.lstrip("_")
-			break
-		cont-=1
+	nameComponents.reverse()
+	banlist=["desktop","org","net","com","app","kde","gnome","gtk","qt"]
+	for component in nameComponents:
+		if component.lower() in banlist:
+			continue
+		if len(component)<2:
+			continue
+		name=component.lstrip("_")
+		break
 	name=name.replace("_zmd",'')
 	return(name)
 #def _componentGetName
